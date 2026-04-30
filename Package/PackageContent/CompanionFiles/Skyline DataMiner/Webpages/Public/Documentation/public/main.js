14359559ec9e286e36cade88f66443121befa9b1
Jarno Lernou <jarno.lernou@skyline.be>
2026-04-30 13:14:33 +0200

    Move all files from dedicated Documentation Project to Package Project.

---
+++ Package/PackageContent/CompanionFiles/Skyline DataMiner/Webpages/Public/Documentation/public/main.js
@@ -0,0 +1,14 @@
+export default {
+	iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/SkylineCommunications/SLC-AS-GetViews',
+      title: 'GitHub'
+    },
+    {
+	  icon: 'book',
+	  href: 'https://docs.dataminer.services',
+	  title: 'DataMiner Docs'
+	}   
+  ]
+}
